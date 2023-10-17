69e78417ece0034168664c4c1c32101230c360b9
Vlad <147541054+kvs23031999@users.noreply.github.com>
2023-10-17 17:46:24 +0300

    second commit

--- src/HWProgram11.java
+++ src/HWProgram11.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Random;
-
+import java.lang.String;
 
 public class HWProgram11{
 
@@ -8,37 +8,24 @@ public class HWProgram11{
 
         Random random = new Random();
         int [] arrayTeam1 = new int[25];
-        int [] arrayTeam2 = new int[25];
-
         for (int index1 = 0; index1 < arrayTeam1.length ; index1++) {
             arrayTeam1[index1] = random.nextInt(22)+18;
 
         }
-        System.out.println(Arrays.toString(arrayTeam1));
-
+        System.out.println(Arrays.toString(arrayTeam1) +" вік 1 команди");
+        int [] arrayTeam2 = new int[25];
         for (int index2 = 0; index2 < arrayTeam2.length;  index2++) {
             arrayTeam2[index2] = random.nextInt(22)+18;
         }
-        System.out.println(Arrays.toString(arrayTeam2));
+        System.out.println(Arrays.toString(arrayTeam1) +" вік 2 команди");
 
+            int sum1= arrayTeam1.length;
+            double avgTeam1 = (double) sum1/ arrayTeam1.length;
+        System.out.println("Середній вік 1 команди" + ": " + sum1);
 
-
-
-        int sum1 = 0;
-        for (int arrElement1: arrayTeam1){
-            sum1 += arrElement1;
-            System.out.println(arrElement1);
-        }
-            double avgTeam1 = (double) sum1 / arrayTeam1.length;
-            System.out.println(avgTeam1 + ": " + sum1);
-
-            int sum2 = 0;
-        for (int arrElement2: arrayTeam2){
-            sum2 += arrElement2;
-            System.out.println(arrElement2);
-        }
-            double avgTeam2 = (double) sum2 / arrayTeam2.length;
-            System.out.println(avgTeam2 + ": " + sum2);
+            int sum2 =arrayTeam2.length;
+            double avgTeam2 = (double) sum2 /arrayTeam2.length;
+        System.out.println("Середній вік 2 команди" + ": " + sum2);
 
         }
 
